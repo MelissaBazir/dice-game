cd4fbb8fccb160a1b1e0bb8d4721d07de5f2ca46
melissa-studi <melissa_bazir@yahoo.fr>
2023-04-18 11:05:23 +0200

    getPlayerIndex function definition

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let globalScores = document.querySelectorAll('.global-score-count')
 let currentScores = document.querySelectorAll('.current-score-count')
 let players = document.querySelectorAll('.player')
+let dice = document.querySelector('.dice')
 const newGameBtn = document.querySelector('#new-game')
 const rollDiceBtn = document.querySelector('#roll-dice')
 const holdBtn = document.querySelector('#hold')
+
+// Display an element on the page
+const displayElement = (element, tagName) => {
+    tagName.innerHTML = element
+}
+
+// Get the index of a player
+const getPlayerIndex = (player) => {
+    return players.indexOf(player)
+}
 
 //reset a score to 0
 const reset = (score) => {
@@ -12,7 +23,7 @@ const reset = (score) => {
 
 // add points to the score of a player
 const addPoints = (points, score, player) => {
-    score[players.indexOf(player)] += points
+    return (score[getPlayerIndex(player)] += points)
 }
 
 // change the active player
@@ -24,18 +35,22 @@ const changePlayer = () => {
 
 // roll dice
 const rollDice = () => {
-    // get a random number between 1 and 6
+    // get a random number between 1 and 6, then display it in dice
     let numberDice = Math.floor(Math.random() * 6) + 1
+    displayElement(numberDice, dice)
     // get the current player
     let player = players[0].classList.contains('.active')
         ? players[0]
         : players[1]
-    // if number not 1, add the number to current score
+    // if number not 1, add the number to current score, then display it
     // if number is 1, set current score to 0 and change player
     if (numberDice !== 1) {
-        addPoints(numberDice, currentScores, player)
+        displayElement(
+            addPoints(numberDice, currentScores, player),
+            currentScores[getPlayerIndex(player)]
+        )
     } else {
-        reset(currentScores[players.indexOf(player)])
+        reset(currentScores[getPlayerIndex(player)])
         changePlayer()
     }
 }
