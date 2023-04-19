7e317eb3837df1366cc08710d1032414225da5c6
melissa-studi <melissa_bazir@yahoo.fr>
2023-04-19 14:07:15 +0200

    adding listener functions

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ let globalScores = document.querySelectorAll('.global-score-count')
 let currentScores = document.querySelectorAll('.current-score-count')
 let players = document.querySelectorAll('.player')
 let dice = document.querySelector('.dice')
+let activePlayer = players[0]
 const newGameBtn = document.querySelector('#new-game')
 const rollDiceBtn = document.querySelector('#roll-dice')
 const holdBtn = document.querySelector('#hold')
 
-// Display an element on the page
+
+// display an element on the page
 const displayElement = (element, tagName) => {
     tagName.innerHTML = element
 }
 
-// Get the index of a player
+// get the index of a player
 const getPlayerIndex = (player) => {
     return players.indexOf(player)
 }
 
 //reset a score to 0
 const reset = (score) => {
-    score = 0
+    return (score = 0)
 }
 
 // add points to the score of a player
@@ -31,6 +33,9 @@ const changePlayer = () => {
     players.forEach((player) => {
         player.classList.toggle('.active')
     })
+    return (activePlayer = players[0].classList.contains('.active')
+        ? players[0]
+        : players[1])
 }
 
 // roll dice
@@ -38,19 +43,43 @@ const rollDice = () => {
     // get a random number between 1 and 6, then display it in dice
     let numberDice = Math.floor(Math.random() * 6) + 1
     displayElement(numberDice, dice)
-    // get the current player
-    let player = players[0].classList.contains('.active')
-        ? players[0]
-        : players[1]
     // if number not 1, add the number to current score, then display it
     // if number is 1, set current score to 0 and change player
     if (numberDice !== 1) {
         displayElement(
-            addPoints(numberDice, currentScores, player),
-            currentScores[getPlayerIndex(player)]
+            addPoints(numberDice, currentScores, activePlayer),
+            currentScores[getPlayerIndex(activePlayer)]
         )
     } else {
-        reset(currentScores[getPlayerIndex(player)])
         changePlayer()
     }
 }
+
+//hold
+const hold = () => {
+    // get the current player
+    let player = players[0].classList.contains('.active')
+        ? players[0]
+        : players[1]
+    // get the current score and send it to global score
+    addPoints(currentScores, globalScores, activePlayer)
+    // set the current score to 0
+    // change player
+}
+
+// on new game click, a new game is initialized
+newGameBtn.addEventListener('click', () => {
+    newGame() // function to be defined
+})
+
+// on roll dice click, the roll dice function is executed
+rollDiceBtn.addEventListener('click', () => {
+    rollDice()
+})
+
+// on hold click, the hold function is executed
+holdBtn.addEventListener('click', () => {
+    hold()
+})
+
+// when a player scores 100 points, he wins => to be defined (modal window)
